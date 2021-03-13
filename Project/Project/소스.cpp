e7a708a934815ed4c35cc398e0a25b51c51e99d2
imyanghw <imyang3163@naver.com>
2021-03-13 21:01:37 +0900

    C언어 연결리스트 활용

--- Project/Project/소스.cpp
+++ Project/Project/소스.cpp
@@ -17,7 +17,14 @@ typedef struct Addbook {
 }addbook;
 
 addbook* createList(); //리스트 초기화 함수
+
 void inputData(addbook*); //리스트 입력 함수
+void printList(addbook*); //리스트 출력 함수
+void findList(addbook*); //리스트 검색 함수
+void deleteList(addbook*); //리스트 삭제 함수
+void selectionSort(addbook*); //리스트 정렬 함수
+int saveData(addbook *); //등록된 회원정보를 파일에 저장하는 함수
+
 
 int main()
 {
@@ -112,7 +119,7 @@ addbook* createList() //공백리스트 생성 및 초기화
 	return node;
 }
 
-void inputData(addbook* head) //리스트에 사용자 추가
+void inputData(addbook* head) //리스트에 노드 추가
 {
 	char _name[MAXNAME]; //이름
 	char _birthday[MAXBIRTHDAY]; //생년월일
@@ -170,4 +177,137 @@ void inputData(addbook* head) //리스트에 사용자 추가
 			temp->link = NULL;
 		}
 	}
+}
+
+void printList(addbook* head) //리스트 노드 출력
+{
+	addbook *tempNode;
+	tempNode = head->link;
+	int num = 1; //주소정보에 등록된 인원을 카운팅할 수
+
+	system("cls");
+
+	if (tempNode == NULL) //저장된 데이터가 비어있다면
+	{
+		printf("현재 등록된 정보가 없습니다. (Press enter to menu)\n");
+		fflush(stdin);
+		_getch();
+	}
+
+	while (tempNode != NULL)
+	{
+		printf("\n<%d>번째 회원\n", num++);
+		printf("이 름: %s\n", tempNode->name);
+		printf("생 년 월 일: %s\n", tempNode->birthday);
+		printf("전 화 번 호 : %s\n", tempNode->phone);
+		printf("주 소: %s\n", tempNode->address);
+
+		tempNode = tempNode->link; //다음 노드로 이동
+		if (tempNode == NULL) {
+			printf("━━━━━━━━━━━━━━\n");
+			printf("마지막 회원입니다(Press enter to menu)");
+		}
+		else {
+			printf("다음 회원 보기(Enter)\n");
+		}
+		fflush(stdin);
+		_getch();
+	}
+}
+
+void findList(addbook *head) //리스트에 있는 내용을 검색
+{
+	int check = 0; //회원을 찾기위함
+	addbook *tempNode;
+	tempNode = head->link;
+	char member[MAXNAME];
+
+	printf("찾으실 회원의 이름을 입력하세요 : ");
+	fflush(stdin);
+	scanf("%s", member);
+
+	while (tempNode != NULL)
+	{
+		if (strcmp(tempNode->name, member) == 0)
+		{
+			printf("━━━━━━━━━━━━━━\n");
+			printf("회원을 찾았습니다.\n");
+			printf("이 름: %s\n", tempNode->name);
+			printf("생 년 월 일: %s\n", tempNode->birthday);
+			printf("전 화 번 호 : %s\n", tempNode->phone);
+			printf("주 소: %s\n", tempNode->address);
+			printf("━━━━━━━━━━━━━━\n");
+			printf("(Press enter to menu)\n");
+
+			fflush(stdin);
+			_getch();
+			check = 1; //회원을 찾음
+			tempNode = NULL; //회원을 찾았으므로 while 루프를 빠져나오기 위한 수행
+		}
+		else //회원이 아닐경우 다음 노드로 이동
+		{
+			tempNode = tempNode->link; //회원을 찾지 못했으면 다음 노드를 검색
+		}
+	}
+
+	if (check == 0) //회원이 없다면
+	{
+		printf("\n 찾으시는 회원정보가 존재하지 않습니다.\n");
+		printf("(Press enter to menu)\n");
+		fflush(stdin);
+		_getch();
+	}
+}
+
+void deleteList(addbook *head) //데이터 삭제
+{
+	int check = 0; //삭제한 데이터가 있는지 체크
+	addbook *currNode; //현재노드를 가리킬 노드생성
+	addbook *prevNode; //이전노드를 가리킬 노드생성
+	char delName[MAXNAME];
+
+	printf("삭제하실 회원의 이름을 입력하세요 : ");
+	fflush(stdin);
+	scanf("%s", delName);
+	
+	prevNode = head; //이전노드를 헤더노드로 설정
+	currNode = head->link; //현재 노드를 첫번째 데이터노드로 설정
+
+	while (currNode != NULL)
+	{
+		if (strcmp(currNode->name, delName) == 0)
+		{
+			printf("<%s> 회원을 삭제합니다.", delName);
+			fflush(stdin);
+			_getch();
+
+			prevNode->link = prevNode->link->link;
+			free(currNode);
+			check = 1; //데이터 삭제완료
+			break; //while반복문 빠져나감
+		}
+		else
+		{
+			prevNode = currNode; //이전노드를 현재 노드로 옮기고 난 후에
+			currNode = currNode->link; //현재노드는 다음 링크 노드로 이동
+		}
+	}
+
+	if (check == 0) //회원이 없다면
+	{
+		printf("\n 삭제하려는 회원정보가 존재하지 않습니다.\n");
+		printf("(Press enter to menu)\n");
+		fflush(stdin);
+		_getch();
+	}
+}
+
+void selectonSort(addbook *head) //선택정렬을 구현하여 이름을 오름차순으로 정렬
+{
+
+}
+
+int saveData(addbook *head)
+{
+
 }
